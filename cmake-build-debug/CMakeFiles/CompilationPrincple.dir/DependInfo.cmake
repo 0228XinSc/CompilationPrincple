b994afd284ea23268712e73e96cd78ed5fc4887e
0228XinSc <1261561427@qq.com>
2021-10-29 14:08:12 +0800

    第二次提交，删除XinSC文件夹

---
+++ cmake-build-debug/CMakeFiles/CompilationPrincple.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/University/3_1/CP/CompilationPrincple/Error.cpp" "D:/University/3_1/CP/CompilationPrincple/cmake-build-debug/CMakeFiles/CompilationPrincple.dir/Error.cpp.obj"
+  "D:/University/3_1/CP/CompilationPrincple/Symbol_Table.cpp" "D:/University/3_1/CP/CompilationPrincple/cmake-build-debug/CMakeFiles/CompilationPrincple.dir/Symbol_Table.cpp.obj"
+  "D:/University/3_1/CP/CompilationPrincple/Syntax_analysis.cpp" "D:/University/3_1/CP/CompilationPrincple/cmake-build-debug/CMakeFiles/CompilationPrincple.dir/Syntax_analysis.cpp.obj"
+  "D:/University/3_1/CP/CompilationPrincple/lexical_analysis.cpp" "D:/University/3_1/CP/CompilationPrincple/cmake-build-debug/CMakeFiles/CompilationPrincple.dir/lexical_analysis.cpp.obj"
+  "D:/University/3_1/CP/CompilationPrincple/main.cpp" "D:/University/3_1/CP/CompilationPrincple/cmake-build-debug/CMakeFiles/CompilationPrincple.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CompilationPrincple.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompilationPrincple.dir/main.cpp.obj"
+  "CMakeFiles/CompilationPrincple.dir/Syntax_analysis.cpp.obj"
+  "CMakeFiles/CompilationPrincple.dir/Symbol_Table.cpp.obj"
+  "CMakeFiles/CompilationPrincple.dir/Error.cpp.obj"
+  "CMakeFiles/CompilationPrincple.dir/lexical_analysis.cpp.obj"
+  "CompilationPrincple.pdb"
+  "CompilationPrincple.exe"
+  "CompilationPrincple.exe.manifest"
+  "libCompilationPrincple.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompilationPrincple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
